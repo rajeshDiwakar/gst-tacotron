9e321faaaefce55a77612a20507dee037f160034
rajesh <rajesh.kr.diwakar@gmail.com>
2020-09-11 16:02:40 +0530

    cleaning download_audio_meta.py , added args

--- tools/download_audio_meta.py
+++ tools/download_audio_meta.py
@@ -1,3 +1,8 @@
+'''
+python download_audio_meta.py --booklist goodreads/books_pdflog.tsv \
+            --start 0 --end 10 --num_results 3 --nofemale
+'''
+
 import os, sys
 import youtube_dl
 from tqdm import tqdm
@@ -5,6 +10,7 @@ import csv
 from termcolor import colored
 import json
 import time
+import argparse
 
 YT_KEY='AIzaSyCCyED1Pv6pGQgm1Qb-6HxoRo0m39OXdpA'
 
@@ -31,21 +37,27 @@ def test_download_idlist():
     inp = sys.argv[1]
     download(inp,False,True,True)
 
-def download_audiobook_meta(file_path):
+def download_audio_meta(args):
     '''
     find audiobooks whose name and author are mentioned in the input file
     '''
+    file_path = args.booklist
+    end = args.end # actual number of rows in books where searching stopped
     with open(file_path) as csvfile:
-        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        dialect = csv.Sniffer().sniff(csvfile.read(10240))
         csvfile.seek(0)
         reader = csv.reader(csvfile, dialect)
         search_items = []
-        for row in reader:
+        for i,row in enumerate(reader):
+            if i < args.start or (args.end>0 and i>=args.end): continue
             if len(row) >= 3:
+                end = i
                 i,book,author = row[:3]
                 # search_items.append('%s %s'%(book,author))
-                search_items.append('%s %s audiobook'%(book,author))
-                search_items.append('%s %s audiobook female voice'%(book,author))
+                if not args.nomale:
+                    search_items.append('%s %s audiobook'%(book,author))
+                if not args.nofemale:
+                    search_items.append('%s %s audiobook female voice'%(book,author))
             else:
                 print('Error: unexpected number of cols: %s'%str(row))
 
@@ -60,23 +72,23 @@ def download_audiobook_meta(file_path):
                             'ignoreerrors':True
                                 })
 
-    meta_file = 'audiobook_meta.jsonl'
+    meta_file = os.path.splitext(file_path)[0]+'_audio_meta_%d-%d.jsonl'%(args.start,end)
     id_cache = []
     if os.path.isfile(meta_file):
-        mv_path = 'audiobook_meta-%s.bk.jsonl'%(time.asctime())
+        mv_path = os.path.splitext(meta_file)[0]+'_%s.jsonl'%(time.asctime())
         print('Moving %s to %s'%(meta_file,mv_path))
         os.rename(meta_file,mv_path)
     with open(meta_file,'w',encoding='utf-8') as f_meta:
         with ydl:
             # searching
-            keep_keys = ['id','webpage_url','channel_id','upload_date','title','subtitles','automatic_captions',
+            keep_keys = ['id','webpage_url','uploader','channel_id','upload_date','title','subtitles','automatic_captions',
                             'duration','view_count','like_count','dislike_count','average_rating'
                           ]
-            num_books = 100
-            num_results = 10
-            print(colored('only using %d search items'%(2*num_books),'red'))
+            # num_books = 100
+            num_results = args.num_results
+            # print(colored('only using %d search items'%(2*num_books),'red'))
             # for item in tqdm(search_items[130:2*num_books]):
-            for item in tqdm(search_items[:2*num_books]):
+            for item in tqdm(search_items):
                 try:
                     result = ydl.extract_info('ytsearch%d:%s'%(num_results, item), download=False)
                     query = {'query':item}
@@ -100,7 +112,7 @@ def download_audiobook_meta(file_path):
                         # Just a video
                         # video = result
                         # raise Exception
-                        print(str(result)[:500] )
+                        print(str(result)[:500].replace('\n',' ') )
                         print('Error in %s'%item)
                         continue
                 except Exception as e:
@@ -110,8 +122,12 @@ def download_audiobook_meta(file_path):
 if __name__=='__main__':
     # file_path = '/home/rajesh/work/limbo/data/yt/500books.tsv'
     # file_path = '/home/rajesh/work/limbo/data/yt/testbooks2.tsv'
-    try:
-        file_path = sys.argv[1]
-    except Exception as e:
-        print('python %s path/to/book.tsv'%(sys.argv[0]))
-    download_audiobook_meta(file_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--booklist',required=True,help='path to booklist tsv')
+    parser.add_argument('--start',default=0,type=int,help='start index (including) of row to process(0 based)')
+    parser.add_argument('--end',default=-1,type=int,help='last index (excluding) of row to process(counting based not the given in the list)')
+    parser.add_argument('--num_results',default=10,type=int,help='number of youtube videos to search')
+    parser.add_argument('--nofemale',action='store_true',default=False,help='set to option to disable explicitly searching for female')
+    parser.add_argument('--nomale',action='store_true',default=False,help='set to option to disable simple audiobook search (male search)')
+    args = parser.parse_args()
+    download_audio_meta(args)
